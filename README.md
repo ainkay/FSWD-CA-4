dc6e8b5ab17313fecdaee8a72187b4a5b61f5b35
ainkay <ain.kay@kalvium.community>
2025-03-19 16:09:53 +0530

    ca-4

---
+++ README.md
@@ -0,0 +1,6 @@
+## TASK 1:
+![alt text](<Screenshot 2025-03-19 154733.png>)
+![alt text](<Screenshot 2025-03-19 154742.png>)
+
+## TASK 2:
+![alt text](<Screenshot 2025-03-19 155942.png>)
---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      
+      <h1>Hospital Management System</h1>
+      
+        
+      <p>
+        Alice Brown
+        30
+        Heart Disease
+         Dr.John Doe
+      
+
+      </p>
+      <p>
+        Bob White
+        25
+        Skin Allergy
+         Dr.John Doe
+      
+
+      </p>
+
+    </>
+  )
+}
+
+export default App
